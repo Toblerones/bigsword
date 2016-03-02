4ac551f94634f3410899c7bcfd1b5e405a0c8d74
Toblerones Lo <toblerones@hotmail.com>
2016-03-03 00:34:23 +0800

    Bigsword core domain with project setting

--- bigsword-eleave-web/src/main/java/com/bigsword/eleave/web/json/response/JsonRefreshResponse.java
+++ bigsword-eleave-web/src/main/java/com/bigsword/eleave/web/json/response/JsonRefreshResponse.java
@@ -7,4 +7,24 @@ public class JsonRefreshResponse extends ResponseBase{
 	private int currentUserId;
 	private String currentUserName;
 	private String system;
+	public int getCurrentUserId() {
+		return currentUserId;
+	}
+	public void setCurrentUserId(int currentUserId) {
+		this.currentUserId = currentUserId;
+	}
+	public String getCurrentUserName() {
+		return currentUserName;
+	}
+	public void setCurrentUserName(String currentUserName) {
+		this.currentUserName = currentUserName;
+	}
+	public String getSystem() {
+		return system;
+	}
+	public void setSystem(String system) {
+		this.system = system;
+	}
+	
+	
 }
